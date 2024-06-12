9448c320c45f4663c1e1add933cbf1d5b2b5ef49
nickokkinidis <nickokkinidis@gmail.com>
2024-06-13 01:02:28 +0300

    some changes

--- music_editor/music_editor.py
+++ music_editor/music_editor.py
@@ -21,8 +21,10 @@ def main():
 
 
 def welcome_user():
+    print("\n - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n")
     print("Welcome to the Music Editor!")
     print("This program will help you edit your music's info.")
+    print("\n - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n")
     check_to_continue()
 
 
@@ -59,19 +61,21 @@ def process_mp3_files(folder_path, new_folder_path):
             source_path = os.path.join(folder_path, filename)
             audio = MP3(source_path, ID3=EasyID3)
 
+            print("\n - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n")
             print(f"Processing: {filename}")
             print(f"Title: {audio.get('title', ['Unknown'])[0]}")
             print(f"Composer: {audio.get('composer', ['Unknown'])[0]}")
             print(f"Artist: {audio.get('artist', ['Unknown'])[0]}")
             print(f"Album: {audio.get('album', ['Unknown'])[0]}")
-
+            print("\n - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n")
+            
             audio['title'] = input("Enter new title: ")
             audio['composer'] = input("Enter new composer: ")
             audio['artist'] = input("Enter new artist: ")
             audio['album'] = input("Enter new album: ")
 
             # Rename the destination file based on the new title
-            new_filename = audio['title'][0] + '.mp3'
+            new_filename = audio['album'][0] + ' - ' + audio['title'][0] + '.mp3'
             destination_path = os.path.join(new_folder_path, new_filename)
 
             # Copy the original file to the new folder
